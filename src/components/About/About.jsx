bc47599ae8de1ba2d523189524df64a8c8b1c9a1
mansoor shah <iqbalmansoor55@gmail.com>
2025-09-22 18:27:20 +0500

    chooseUs

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import Achievements from "../OurProjects/Achievements";
 import Stages from "../OurProjects/Stages";
 import Footer from "../Footer/Footer";
-import ChooseUs from "./Chooseus.JSx";
-
+import ChooseUs from "./ChooseUs";
 function About() {
   const cards = [
     {
